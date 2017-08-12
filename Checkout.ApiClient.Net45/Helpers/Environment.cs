441eb60ba4e1b39fd77508a0d0416c8fd45fdb35
slyrat <sly.rat.peon@gmail.com>
2017-08-12 17:18:50 +0100

    Get client in place

---
+++ Checkout.ApiClient.Net45/Helpers/Environment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.Helpers
+{
+    public enum Environment
+    {
+        Undefined,
+        Sandbox,
+        Live, 
+        LocalHost
+    }
+}
